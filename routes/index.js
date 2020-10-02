b4b0c723bb5d2831236c46cf56d04de56689705a
Balaji Vengatesh M <balajimurugesan.2016@gmail.com>
2020-10-03 02:30:48 +0530

    more dynamic changes

--- routes/index.js
+++ routes/index.js
@@ -11,10 +11,22 @@ module.exports = () => {
 
         const dbservice = new DBservice(hana);
         const personservice = new PersonService(dbservice);
-        var personaldata = ""
+        var personaldata = "";
+        var generalinfo = "";
         try {
-            personaldata = await personservice.getGeneralInfo('DANIEL HUFFMAN');
+            personaldata = await personservice.getPersonalData('DANIEL HUFFMAN');
             personaldata = personaldata[0];
+            if (personaldata.ID) {
+                generalinfo = await personservice.getGeneralInfo(personaldata.ID);
+                generalinfo = generalinfo[0]
+                workexperiences = await personservice.getworkexperiences(personaldata.ID);
+                educationlist = await personservice.geteducation(personaldata.ID);
+                portfoliolist = await personservice.getportfolio(personaldata.ID);
+                referencelist = await personservice.getreferencelist(personaldata.ID);
+                console.log(referencelist);
+            }
+
+
         } catch (err) {
             console.log(err);
 
@@ -22,7 +34,13 @@ module.exports = () => {
 
 
         resp.render('index', {
-            personaldata
+            personaldata,
+            generalinfo,
+            workexperiences,
+            educationlist,
+            portfoliolist,
+            referencelist
+
         });
 
     })
--- services/personservice.js
+++ services/personservice.js
@@ -13,12 +13,50 @@ module.exports = class PersonService {
 
     }
 
-    async getGeneralInfo(name) {
+    async getPersonalData(name) {
 
         var sql = `SELECT ID,NAME,DESC FROM D4A4022E7221468688718C057DEA728C.PERSONAL_DATA  WHERE NAME =\'${name}\'`;
 
         return await this.hanaservice.fetchrecordsbyquery(sql);
     }
+
+    async getGeneralInfo(id) {
+
+        var sql = `SELECT * FROM D4A4022E7221468688718C057DEA728C.GENERAL_INFO  WHERE ID = ${id}`;
+
+        return await this.hanaservice.fetchrecordsbyquery(sql);
+    }
+
+    async getworkexperiences(id) {
+        var sql = `SELECT * FROM D4A4022E7221468688718C057DEA728C.WORK_EXPERIENCE  WHERE ID = ${id}`;
+
+        return await this.hanaservice.fetchrecordsbyquery(sql);
+
+    }
+
+    async geteducation(id) {
+        var sql = `SELECT * FROM D4A4022E7221468688718C057DEA728C.EDUCATION  WHERE ID = ${id}`;
+
+        return await this.hanaservice.fetchrecordsbyquery(sql);
+
+    }
+
+    async getportfolio(id) {
+        var sql = `SELECT * FROM D4A4022E7221468688718C057DEA728C.PORTFOLIO  WHERE ID = ${id}`;
+        return await this.hanaservice.fetchrecordsbyquery(sql);
+
+    }
+
+    async getreferencelist(id) {
+        var sql = `SELECT * FROM D4A4022E7221468688718C057DEA728C.REFERENCELIST  WHERE ID = ${id}`;
+        return await this.hanaservice.fetchrecordsbyquery(sql);
+
+    }
+
+
+
+
+
 
 
 
